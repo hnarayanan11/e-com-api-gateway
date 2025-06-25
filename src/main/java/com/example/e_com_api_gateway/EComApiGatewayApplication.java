52e0a561ea2c32f14538a2f770b69cd9c7d51e38
hnarayanan11 <hnarayanan11@gmail.com>
2025-06-25 11:52:16 +0530

    Initial Commit

---
+++ src/main/java/com/example/e_com_api_gateway/EComApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_com_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComApiGatewayApplication.class, args);
+	}
+
+}
