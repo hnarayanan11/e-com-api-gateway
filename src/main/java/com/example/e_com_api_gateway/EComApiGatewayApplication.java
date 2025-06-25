0fd74251612cd31448a94127c44fb22d1d02083b
hnarayanan11 <hnarayanan11@gmail.com>
2025-06-25 12:25:41 +0530

    Eureka config changes

--- src/main/java/com/example/e_com_api_gateway/EComApiGatewayApplication.java
+++ src/main/java/com/example/e_com_api_gateway/EComApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.example.e_com_api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EComApiGatewayApplication {
 
 	public static void main(String[] args) {
